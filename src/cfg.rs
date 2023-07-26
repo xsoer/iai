09fd54154886352a67ad932f7be1c5389ed5cd5e
zxc <zhangxicheng@ipplus360.com>
2023-07-26 16:09:53 +0800

    add request

---
+++ src/cfg.rs
@@ -0,0 +1,3 @@
+pub const URL: &str = "https://api.openai.com/v1/chat/completions";
+
+pub const API_KEY: &str = "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";
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-
+use anyhow;
 use reqwest;
+use reqwest::header;
+use reqwest::header::HeaderMap;
+use serde_json::json;
+
+mod cfg;
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let body = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
+async fn main() -> anyhow::Result<()> {
+    completions().await?;
 
-    println!("body = {:?}", body);
-    println!("Hello, world!");
-    return Ok(())
+    Ok(())
+}
+
+async fn completions() -> anyhow::Result<()> {
+    let client = reqwest::Client::new();
+    let payload = json!({
+        "model":"gpt-4-0613",
+        "messages":[{"role":"system","content":"You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: 2021-09-01. Current date: 2023-07-24"},{"role":"user","content":"btc"}],
+        "stream":true
+    });
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::AUTHORIZATION,
+        format!("Bearer  {}", cfg::API_KEY).parse().unwrap(),
+    );
+
+    let rsp = client
+        .post(cfg::URL)
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await?;
+    let content = rsp.text().await?;
+    // println!("rsp {:#?}", rsp);
+    println!("content {:#?}", content);
+    Ok(())
 }
